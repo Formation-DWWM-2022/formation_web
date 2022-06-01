461c05a4ee83bb0a018615deacd3acf417250c7b
ElliahVD <102582259+ElliahVD@users.noreply.github.com>
2022-06-01 13:58:35 +0200

    exo en FR

--- BDDSQL/Exercice/SQL_exercise_01/1_questions.sql
+++ BDDSQL/Exercice/SQL_exercise_01/1_questions.sql
@@ -1,21 +1,21 @@
 -- LINK: https://en.wikibooks.org/wiki/SQL_Exercises/The_computer_store
--- 1.1 Select the names of all the products in the store.
--- 1.2 Select the names and the prices of all the products in the store.
--- 1.3 Select the name of the products with a price less than or equal to $200.
--- 1.4 Select all the products with a price between $60 and $120.
--- 1.5 Select the name and price in cents (i.e., the price must be multiplied by 100).
--- 1.6 Compute the average price of all the products.
--- 1.7 Compute the average price of all products with manufacturer code equal to 2.
--- 1.8 Compute the number of products with a price larger than or equal to $180.
--- 1.9 Select the name and price of all products with a price larger than or equal to $180, and sort first by price (in descending order), and then by name (in ascending order).
--- 1.10 Select all the data from the products, including all the data for each product's manufacturer.
--- 1.11 Select the product name, price, and manufacturer name of all the products.
--- 1.12 Select the average price of each manufacturer's products, showing only the manufacturer's code.
--- 1.13 Select the average price of each manufacturer's products, showing the manufacturer's name.
--- 1.14 Select the names of manufacturer whose products have an average price larger than or equal to $150.
--- 1.15 Select the name and price of the cheapest product.
--- 1.16 Select the name of each manufacturer along with the name and price of its most expensive product.
--- 1.17 Add a new product: Loudspeakers, $70, manufacturer 2.
--- 1.18 Update the name of product 8 to "Laser Printer".
--- 1.19 Apply a 10% discount to all products.
--- 1.20 Apply a 10% discount to all products with a price larger than or equal to $120.
+-- 1.1 Sélectionnez les noms de tous les produits du magasin.
+-- 1.2 Sélectionnez les noms et les prix de tous les produits du magasin.
+-- 1.3 Sélectionnez le nom des produits dont le prix est inférieur ou égal à 200 $.
+-- 1.4 Sélectionnez tous les produits dont le prix se situe entre 60 $ et 120 $.
+-- 1.5 Sélectionnez le nom et le prix en centimes (c'est-à-dire que le prix doit être multiplié par 100).
+-- 1.6 Calculer le prix moyen de tous les produits.
+-- 1.7 Calculer le prix moyen de tous les produits dont le code fabricant est égal à 2.
+-- 1.8 Calculer le nombre de produits dont le prix est supérieur ou égal à 180 $.
+-- 1.9 Sélectionnez le nom et le prix de tous les produits dont le prix est supérieur ou égal à 180 $, et triez d'abord par prix (par ordre décroissant), puis par nom (par ordre croissant).
+-- 1.10 Sélectionnez toutes les données des produits, y compris toutes les données du fabricant de chaque produit.
+-- 1.11 Sélectionnez le nom du produit, le prix et le nom du fabricant de tous les produits.
+-- 1.12 Sélectionnez le prix moyen des produits de chaque manufacturier en n'indiquant que le code du manufacturier.
+-- 1.13 Sélectionnez le prix moyen des produits de chaque fabricant, en indiquant le nom du fabricant.
+-- 1.14 Sélectionnez les noms de fabricants dont les produits ont un prix moyen supérieur ou égal à 150 $.
+-- 1.15 Sélectionnez le nom et le prix du produit le moins cher.
+-- 1.16 Sélectionnez le nom de chaque fabricant ainsi que le nom et le prix de son produit le plus cher.
+-- 1.17 Ajouter un nouveau produit : Haut-parleurs, 70 $, fabricant 2.
+-- 1.18 Mettez à jour le nom du produit 8 en "Imprimante laser".
+-- 1.19 Appliquer une remise de 10% sur tous les produits.
+-- 1.20 Appliquez une remise de 10 % sur tous les produits dont le prix est supérieur ou égal à 120 $.
