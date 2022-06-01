a6fa609e82f496fc3f25f6e7fceb5bdb10e6c74f
ElliahVD <102582259+ElliahVD@users.noreply.github.com>
2022-06-01 14:00:39 +0200

    FR

--- BDDSQL/Exercice/SQL_exercise_02/2_questions.sql
+++ BDDSQL/Exercice/SQL_exercise_02/2_questions.sql
@@ -1,23 +1,23 @@
 -- LINK : https://en.wikibooks.org/wiki/SQL_Exercises/Employee_management
--- 2.1 Select the last name of all employees.
--- 2.2 Select the last name of all employees, without duplicates.
--- 2.3 Select all the data of employees whose last name is "Smith".
--- 2.4 Select all the data of employees whose last name is "Smith" or "Doe".
--- 2.5 Select all the data of employees that work in department 14.
--- 2.6 Select all the data of employees that work in department 37 or department 77.
--- 2.7 Select all the data of employees whose last name begins with an "S".
--- 2.8 Select the sum of all the departments' budgets.
--- 2.9 Select the number of employees in each department (you only need to show the department code and the number of employees).
--- 2.10 Select all the data of employees, including each employee's department's data.
--- 2.11 Select the name and last name of each employee, along with the name and budget of the employee's department.
--- 2.12 Select the name and last name of employees working for departments with a budget greater than $60,000.
--- 2.13 Select the departments with a budget larger than the average budget of all the departments.
--- 2.14 Select the names of departments with more than two employees.
--- 2.15 Very Important - Select the name and last name of employees working for departments with second lowest budget.
--- 2.16  Add a new department called "Quality Assurance", with a budget of $40,000 and departmental code 11. 
--- And Add an employee called "Mary Moore" in that department, with SSN 847-21-9811.
--- 2.17 Reduce the budget of all departments by 10%.
--- 2.18 Reassign all employees from the Research department (code 77) to the IT department (code 14).
--- 2.19 Delete from the table all employees in the IT department (code 14).
--- 2.20 Delete from the table all employees who work in departments with a budget greater than or equal to $60,000.
--- 2.21 Delete from the table all employees.
+-- 2.1 Sélectionnez le nom de famille de tous les employés.
+-- 2.2 Sélectionnez le nom de famille de tous les employés, sans doublons.
+-- 2.3 Sélectionnez toutes les données des employés dont le nom de famille est "Smith".
+-- 2.4 Sélectionnez toutes les données des employés dont le nom de famille est "Smith" ou "Doe".
+-- 2.5 Sélectionnez toutes les données des employés qui travaillent dans le département 14.
+-- 2.6 Sélectionnez toutes les données des employés qui travaillent dans le département 37 ou le département 77.
+-- 2.7 Sélectionnez toutes les données des employés dont le nom de famille commence par un "S".
+-- 2.8 Sélectionnez la somme de tous les budgets des départements.
+-- 2.9 Sélectionnez le nombre d'employés dans chaque département (vous n'avez qu'à indiquer le code du département et le nombre d'employés).
+-- 2.10 Sélectionnez toutes les données des employés, y compris les données du service de chaque employé.
+-- 2.11 Sélectionnez le nom et le prénom de chaque employé, ainsi que le nom et le budget du service de l'employé.
+-- 2.12 Sélectionnez le nom et le nom de famille des employés travaillant pour les services dont le budget est supérieur à 60 000 $.
+-- 2.13 Sélectionnez les départements dont le budget est supérieur au budget moyen de tous les départements.
+-- 2.14 Sélectionnez les noms des départements comptant plus de deux employés.
+-- 2.15 Très important - Sélectionnez le nom et le prénom des employés travaillant pour les départements avec le deuxième budget le plus bas.
+-- 2.16 Ajouter un nouveau département appelé "Assurance qualité", avec un budget de 40 000 $ et le code départemental 11.
+-- Et ajoutez un employé appelé "Mary Moore" dans ce département, avec le SSN 847-21-9811.
+-- 2.17 Réduire le budget de tous les départements de 10 %.
+-- 2.18 Réaffecter tous les employés du service Recherche (code 77) au service informatique (code 14).
+-- 2.19 Supprimer du tableau tous les employés du service informatique (code 14).
+-- 2.20 Supprimer du tableau tous les employés qui travaillent dans des services dont le budget est supérieur ou égal à 60 000 $.
+-- 2.21 Supprimer du tableau tous les employés.
